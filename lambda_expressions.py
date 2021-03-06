7df06b27592af7a03ebd3891542e59d182f6e196
Stanley <2001stany@gmail.com>
2021-03-06 09:02:37 +0300

    add: lambda expression

---
+++ lambda_expressions.py
@@ -0,0 +1,12 @@
+# normal function
+# def adding(num1, num2):
+#     print(num1 + num2)
+
+# adding(3, 99)
+
+
+# lambda/anonymous function
+
+adding = lambda num1, num2: print(num1 + num2)
+
+adding(3,8)
